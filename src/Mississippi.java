3dc9d0465d675c435108a3a8b1560fd4a1c49966
yanchemm000 <yanchemm000@CAM-LAB-29.bgsd.k12.wa.us>
2019-09-13 07:44:53 -0700

    mississippi

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -4,6 +4,10 @@ public class Mississippi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	public static void letterM() {
+		System.out.println("M        M");
+		
+	}
 	}
 
 }
