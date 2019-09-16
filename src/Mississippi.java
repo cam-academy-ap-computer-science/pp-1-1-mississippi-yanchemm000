d699fd12715c32d66c72127f96632a9ba30f6eda
yanchemm000 <yanchemm000@CAM-LAB-29.bgsd.k12.wa.us>
2019-09-16 07:45:12 -0700

    done

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -3,11 +3,64 @@ public class Mississippi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	public static void letterM() {
-		System.out.println("M        M");
+		firstletter();
+		System.out.println("");
+		i();
+		System.out.println("");
+		s();
+		System.out.println("");
+		s();
+		System.out.println("");
+		i();
+		System.out.println("");
+		s();
+		System.out.println("");
+		s();
+		System.out.println("");
+		i();
+		System.out.println("");
+		p();
+		System.out.println("");
+		p();
+		System.out.println("");
+		i();
 		
 	}
+	public static void firstletter() {
+		System.out.println("M        M");
+		System.out.println("M M    M M");
+		System.out.println("M  M  M  M");
+		System.out.println("M   M    M");
+		System.out.println("M        M");
+		System.out.println("M        M");
+		System.out.println("M        M");
 	}
-
+	public static void i() {
+		System.out.println("IIIIIIIIII");
+		System.out.println("     I    ");
+		System.out.println("     I    ");
+		System.out.println("     I    ");
+		System.out.println("     I    ");
+		System.out.println("     I    ");
+		System.out.println("IIIIIIIIII");
+	}
+	public static void s() {
+		System.out.println(" SSSSSSSS ");
+		System.out.println("S         ");
+		System.out.println("S         ");
+		System.out.println(" SSSSSSSS ");
+		System.out.println("         S");
+		System.out.println("         S");
+		System.out.println(" SSSSSSSS ");
+	}
+	public static void p() {
+		System.out.println("PPPPPPPPP");
+		System.out.println("P        P");
+		System.out.println("P        P");
+		System.out.println("PPPPPPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+	}
 }
+
